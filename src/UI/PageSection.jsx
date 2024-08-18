032e46ecddfe7281991db43184ea54bbfaf62716
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-08-18 16:29:00 +0300

    Added Profile Content

--- src/UI/PageSection.jsx
+++ src/UI/PageSection.jsx
@@ -6,7 +6,7 @@ export default function PageSection ({title, classes, children}) {
     const { theme } = useContext(ThemeCtx);
 
     return <section className={"p-2 w-full " + classes}>
-        <h2 className={`text-xl font-semibold ${theme === 'light' ? 'text-stone-600' : 'text-stone-300'} mb-4`}>{title}</h2>
+        <h2 className={`text-xl underline font-semibold ${theme === 'light' ? 'text-stone-600' : 'text-stone-300'} mb-4`}>{title}</h2>
         {children}
     </section>
 }
